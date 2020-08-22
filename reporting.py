e5af54569823285b6a0badfdb8db9cb62550ddf9
Clément Notin <clement@notin.org>
2020-08-22 19:03:25 +0200

    Use CDN for CSS

--- reporting.py
+++ reporting.py
@@ -7,15 +7,14 @@ from jinja2 import Template
 
 
 def generate_reporting(target_user, likes):
-    # if we don't already have the output folder with CSS files, then create it
+    # if we don't already have the output folder then create it
     # (otherwise we only re-create the HTML page)
     output_dir = f"html_output_{target_user}"
     if not os.path.isdir(output_dir):
         os.mkdir(output_dir)
-        shutil.copytree("templates/css", output_dir + "/css")
 
     # load template
-    with open("templates/index.html", "r", encoding="utf-8") as f:
+    with open("index.html", "r", encoding="utf-8") as f:
         index_tpl = Template(f.read())
     # process template with likes data
     index_data = index_tpl.render(likes=likes)
